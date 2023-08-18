908d2718b4dfbc3bdee4f22bccfc247217f1211d
ViktoriaDzunusova <vikenti1996@gmail.com>
2023-08-18 14:58:22 +0300

    seconddd commit

---
+++ lesson2.js
@@ -0,0 +1,37 @@
+let cars = [
+  "Bentley",
+  "Jaguar",
+  "Lotus",
+  "Ford",
+  "BMW",
+  "MG",
+  "Rover",
+  "Riley",
+  "Fiat",
+  "Geely"
+];
+console.log(cars)
+let users = [
+  { name:`Vika`, username:`Vikas`,password:123},
+  { name:`Oleg`, username:`Oleg3`,password:333},
+  { name:`Misha`, username:`Mishal`,password:443},
+  { name:`Kira`, username:`KiraSD`,password:4444},
+  { name:`Vita`, username:`VitalER`,password:5555},
+  { name:`Valya`, username:`ValyaBG`,password:656},
+  { name:`Mark`, username:`Mark45`,password:4545},
+  { name:`Kolya`, username:`Kolya56`,password:5656},
+  { name:`Nina`, username:`Nina23`,password:4309},
+  { name:`Tom`, username:`Tomas19`,password:19191}
+]
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+
+
