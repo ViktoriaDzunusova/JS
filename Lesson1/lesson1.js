b4a0dd4e2aff616ffc6fa6a706e6f469e1c4720b
ViktoriaDzunusova <vikenti1996@gmail.com>
2023-08-18 14:07:40 +0300

    second commit

---
+++ Lesson1/lesson1.js
@@ -0,0 +1,52 @@
+let a1;
+a1='hello';
+console.log(a1);
+let a2;
+a2='owu';
+console.log(a2);
+let a3;
+a3='com';
+console.log(a3);
+let a4;
+a4= 'ua';
+console.log(a4);
+let a5;
+a5=1;
+console.log(a5);
+let a6;
+a6= 10;
+console.log(a6);
+let a7;
+a7=-999;
+console.log(a7);
+let a8;
+a8=123;
+console.log(a8);
+let a9;
+a9=3.14;
+console.log(a9);
+let b1;
+b1=2.7;
+console.log(b1);
+let b3;
+b3=16;
+console.log(b3);
+let b4
+b4=true;
+console.log(b4);
+let b5;
+b5=false;
+console.log(b5);
+let person ={
+  firstName:`Dzunusova`,
+  middleName:`Viktoria`,
+  lastName:`Olegivna`
+}
+console.log(person)
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
+
